8bce88c0407ab76dd3f82200c73703d239c39101
jelouani <jelouaniabdelghani@gmail.com>
2023-05-09 02:04:54 +0100

    updates

--- code_projet/tailwind.config.js
+++ code_projet/tailwind.config.js
@@ -2,10 +2,17 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'blueColor' : '#2a68ff',
+        'greyIsh' : '#1ff4f8',
+        'cardShadow' : '#f7f8f9',
+        'textColor' : '#252b36',
+      }
+    },
   },
   plugins: [],
 }
