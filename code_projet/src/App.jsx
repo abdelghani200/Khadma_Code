3e2528aeae1bbe5bf3b406155c5446cd5c3a673e
jelouani <jelouaniabdelghani@gmail.com>
2023-05-09 02:04:49 +0100

    appel component

--- code_projet/src/App.jsx
+++ code_projet/src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Recherche from "./Components/RechercheDiv/Recherche";
+import Khadma from "./Components/KhadmaDiv/Khadma";
+import Valeur from "./Components/ValeurDiv/Valeur";
+import Footer from "./Components/FooterDiv/Footer";
 
 const App = ()=> {
   return (
-    <div>App</div>
+    <div className="w-[85%] m-auto bg-white"> 
+      <Navbar/>
+      <Recherche/>
+      <Khadma/>
+      <Valeur/>
+      <Footer/>
+    </div>
   )
 }
 
